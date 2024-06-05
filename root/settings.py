c435c2f2e2c7dcc3ccbf1d1f6b06b3a29b326f30
samariddin <samariddin0304@gmail.com>
2024-06-05 21:46:31 +0500

    changes

--- root/settings.py
+++ root/settings.py
@@ -60,10 +60,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'root.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'HOST': os.getenv("DB_HOST"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
