8285e63476fd57ca134208ce8cfd077fe7cb1031
samariddin <samariddin0304@gmail.com>
2024-06-08 14:09:29 +0500

    changes

--- tlg_bot/main.py
+++ tlg_bot/main.py
@@ -29,31 +29,31 @@ async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
     await bot.delete_webhook(drop_pending_updates=True)
 
 
-async def main() -> None:
+def main() -> None:
     dp = Dispatcher()
     dp.include_router(private_handler_router)
-    # dp.startup.register(on_startup)
-    # dp.shutdown.register(on_shutdown)
-    #
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    # app = web.Application()
-    #
-    # webhook_requests_handler = SimpleRequestHandler(
-    #     dispatcher=dp,
-    #     bot=bot,
-    #     secret_token=conf.bot.WEBHOOK_SECRET,
-    # )
-    # webhook_requests_handler.register(app, path=conf.bot.WEBHOOK_PATH)
-    #
-    # setup_application(app, dp, bot=bot)
-    #
-    # web.run_app(app, host=conf.bot.WEB_SERVER_HOST, port=conf.bot.WEB_SERVER_PORT)
+    app = web.Application()
 
-    await dp.start_polling(bot)
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=conf.bot.WEBHOOK_SECRET,
+    )
+    webhook_requests_handler.register(app, path=conf.bot.WEBHOOK_PATH)
+
+    setup_application(app, dp, bot=bot)
+
+    web.run_app(app, host=conf.bot.WEB_SERVER_HOST, port=conf.bot.WEB_SERVER_PORT)
+    #
+    # await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    # main()
-    asyncio.run(main())
+    main()
+    # asyncio.run(main())
